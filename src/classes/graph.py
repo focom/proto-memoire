af4c9558a1b1d873415951b0e566a733de38e8bd
Pierre Valentin <me@pierrevalentin.org>
2019-01-19 17:11:18 -0500

    working query to get grade of node

--- src/classes/graph.py
+++ src/classes/graph.py
@@ -8,8 +8,51 @@ from neo4j import *
 
 class Graph:
   def __init__(self,adress,user,password):
-    self.driver = GraphDatabase.driver("adress", auth=("neo4j", "root"))
-  
-  def updateGraph():
-    # Take care of small part of tree
+    self.driver = GraphDatabase.driver(adress, auth=(user, password))
 
+  def get_db(self):
+    return self.driver.session()
+
+  def getScoreNode(self,id_node):
+    with self.driver.session() as session:
+      session.read_transaction(self.scoreNode,id_node)
+  
+  def scoreNode(self,tx,id_node):
+    db = self.get_db()
+    query = f"MATCH (n:Chapitre) where n.id_node='{id_node}' return n"
+    print (query)
+    result = db.run(query)
+    # tx.run("MATCH (n:Chapitre) where n.id_chap=$id_node return n",id_node)
+    # print('hello')
+    # print(vars(result))
+    for record in result:
+      print(record[0]._properties['grade'])
+      # print (record[0]._properties['grade'])
+  
+  def updateGraph(self,tx):
+    # Take care of small part of tree
+    # select 8 and 9 and apply to 3.
+
+    score8 = 0
+    score9 = 0
+    score3 = 0
+    history3 = 0
+
+    scoreSmall = [0,0,0]
+
+    for record in tx.run("MATCH (n:Chapitre) return n where n.id_chap=8"):
+      print (record[0]._properties['grade'])
+    for record in tx.run("MATCH (n:Chapitre) return n where n.id_chap=9"):
+      print (record[0]._properties['grade'])
+    for record in tx.run("MATCH (n:Chapitre) return n where n.id_chap=3"):
+      print (record[0]._properties['grade'])
+  
+  def runUpdateGraph(self):
+    with self.driver.session() as session:
+      session.read_transaction(self.updateGraph)
+
+if (__name__ == '__main__'):
+  graph = Graph('bolt://localhost:7687','neo4j','root')
+  # graph.getScoreNode({'id_node':'4'})
+  graph.getScoreNode('4')
+    
