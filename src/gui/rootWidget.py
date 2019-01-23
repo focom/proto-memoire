0dfa0dca69345da7b1f4084cecfa51e40020cfe2
Pierre Valentin <me@pierrevalentin.org>
2019-01-23 12:34:52 -0500

    wip

--- src/gui/rootWidget.py
+++
@@ -1,7 +0,0 @@
-import tkinter
-
-class RootWidget:
-  def __init__ (self):
-    root = tkinter.Tk()
-    root.mainloop()
-   
--- src/main.py
+++ src/main.py
@@ -1,20 +1,6 @@
 # from classes.game import *
 from tkinter import *
 from classes.personality import Personality
+from classes.main import *
 if ( __name__ == '__main__'):
-    guiRoot = Tk()
-    guiRoot.title('Demande de personalité')
-    guiRoot.geometry('1000x400+400+400')
-
-
-
-    Frame1 = Frame(guiRoot,borderwidth=2,relief=GROOVE,width=400,height=400)
-    # Frame1.grid(column=0, row=0)
-    # Création d'un widget Button (bouton Lancer)
-    context = Personality()
-    BoutonLancer = Button(guiRoot, command= lambda: context.calculatePersonality(guiRoot),text ='Lancer')
-    # Positionnement du widget avec la méthode pack()
-    BoutonLancer.grid(column=0, row=0)
-
-
-    guiRoot.mainloop()
+    main = Main()
